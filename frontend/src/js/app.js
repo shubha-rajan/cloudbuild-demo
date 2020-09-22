417abc152131dab8405e2f3b7e6b8ec53e8175d8
Shubha Rajan <shubharajan94@gmail.com>
2020-09-21 18:17:39 -0700

    add code to connect frontend and backend

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,9 +1,6 @@
 require('../css/main.css');
-require('slick-carousel/slick/slick.css');
-require('slick-carousel/slick/slick-theme.css');
 require('lightbox2/dist/css/lightbox.min.css');
 
 window.$ = require('jquery');
-require('slick-carousel')
-require('lightbox2')
+require('lightbox2');
 require('./gallery.js');
--- frontend/src/js/gallery.js
+++ frontend/src/js/gallery.js
@@ -1,10 +1,14 @@
-$(document).ready(function () {
-    $('.slick-slider').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-});
+const axios = require('axios').default;
+const catApi = 'http://localhost:5000';
+
+
+// $(document).ready(function () {
+//     let htmlString = '';
+//     for (let i = 0; i < 10; i++) {
+//         axios.get(`${catApi}/cat`).then((resp) => {
+//             htmlString += `<a href="${resp.data}" data-lightbox="cat-img"><img src=${resp.data}</a>`;
+//             $('.gallery').html(htmlString);
+//         })
+//     }
+// });
+
